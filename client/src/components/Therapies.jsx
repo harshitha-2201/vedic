438f6e67c9fae1e6f30bab713facd9f9f314d84c
Sampath Akkina <harshitha.akkina22@gmail.com>
2024-11-06 21:39:40 +0530

    connit

--- client/src/components/Therapies.jsx
+++ client/src/components/Therapies.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import elakizhi from '../assets/elakizhi.jpg'; 
 import dhanyamlaDhara from '../assets/dhanyamlaDhara.jpg';
 import PadaAbhyanga from '../assets/PadaAbhyanga.jpg';
