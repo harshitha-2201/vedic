3420bb6f8109fc49dc1c2b0d1d3b8bda2c6af980
Sai Harshitha Akkina <120376983+harshitha-2201@users.noreply.github.com>
2024-11-06 16:50:08 +0530

    Update Login.jsx
    backend login changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:4000/api/auth/login', { email, password });
+      const response = await axios.post('https://appointment-booking-server-j4yg.onrender.com/api/auth/login', { email, password });
       console.log(response.data);
       setError('');
       
