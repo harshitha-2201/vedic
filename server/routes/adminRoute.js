7e5dee63ac52f2ed6f5f11679b8c5de616a06f67
Sai Harshitha Akkina <120376983+harshitha-2201@users.noreply.github.com>
2025-02-13 18:08:26 +0530

    Update adminRoute.js

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getAppointments, updateAppointmentStatus, updateClientProfile } = require('../controllers/adminController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authmiddleware');
 
 
 // Route for fetching appointments
